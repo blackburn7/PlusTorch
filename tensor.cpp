bce5a1a390f9c66d3af4256bc68d545839b8ed19
blackburn <matthewalexanderb@gmail.com>
2024-05-01 17:26:48 -0400

    more tensor features, going to add more generators

--- tensor.cpp
+++ tensor.cpp
@@ -10,27 +10,28 @@ void Tensor::randn() {
 
 
   std::normal_distribution<double> distribution(0, 1);
-  // for (int b = 0; b < this->B; ++b) {
-  //   for (int t = 0; t < this->T; ++t) {
-  //       for (int c = 0; c < this->C; ++c) {
-  //         this->data[b * this->T * this->C + t * this->C + c] = distribution(gen);
-  //     }
-  //   }
-  // }
+  for (int b = 0; b < this->B; ++b) {
+    for (int t = 0; t < this->T; ++t) {
+        for (int c = 0; c < this->C; ++c) {
+          this->data[b * this->T * this->C + t * this->C + c] = distribution(gen);
+      }
+    }
+  }
 
 }
 
 
 
 
-void Tensor::print() {
-  for (int b = 0; b < this->B; ++b) {
-    for (int t = 0; t < this->T; ++t) {
-        for (int c = 0; c < this->C; ++c) {
-            std::cout << this->data[b * this->T * this->C + t * this->C + c] << " ";
+std::ostream& operator<<(std::ostream& os, const Tensor& tensor) {
+    for (int b = 0; b < tensor.B; ++b) {
+        for (int t = 0; t < tensor.T; ++t) {
+            for (int c = 0; c < tensor.C; ++c) {
+                os << tensor.data[b * tensor.T * tensor.C + t * tensor.C + c] << " ";
+            }
+            os << std::endl; // Newline after each 'c' loop iteration
         }
-        std::cout << std::endl;  // Newline after each 'c' loop iteration
+        os << std::endl; // Additional newline after each 'b' loop iteration
     }
-    std::cout << std::endl;  // Additional newline after each 'b' loop iteration
-  }
+    return os;
 }
--- tensor.h
+++ tensor.h
@@ -1,4 +1,6 @@
 
+#include <tuple>
+
 #ifndef MYHEADER_H
 #define MYHEADER_H
 class Tensor {
@@ -10,16 +12,33 @@ public:
   int T;
   int C;
 
-  Tensor(int B, int T, int C): B(B), T(T), C(C) {
-    data = new float[B*T*C];
+  Tensor(int B, int T, int C, float* data = nullptr): B(B), T(T), C(C) {
+    if (!data) data = new float[B*T*C];
+  }
+
+  // Copy constructor
+  Tensor(const Tensor& other) : B(other.B), T(other.T), C(other.C) {
+      int size = B * T * C;
+      data = new float[size];
+      std::copy(other.data, other.data + size, data);
+  }
+
+  // Destructor
+  ~Tensor() {
+      delete[] data;
   }
 
   // initializers
   void randn();
+  void arange();
 
 
   // helpers
-  void print();
+  friend std::ostream& operator<<(std::ostream& os, const Tensor& tensor);
+
+  std::tuple<int, int, int> shape() const {
+      return std::make_tuple(B, T, C);
+  }
 
 
 };
